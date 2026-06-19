62e874f1eb52df256f0ee736167f5f561f4fdcae
sonutechgeek <chaudharysonu200212@gmail.com>
2026-06-19 06:59:47 +0530

    window 2

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -22,7 +23,32 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
-        return ["message"=>"store method called"];
+    //    $data = $request->all();
+    //    return response()->json($data)->setStatusCode(201);
+    // $data=$request->only("name");
+    // $data=$request->only("name","email","password");
+    // return response()->json([
+    //     "message"=>"Store User Successfully",
+    //     "data"=>[
+    //         "name"=>$data["name"],
+    //         "email"=>$data["email"],
+    //         "password"=>$data["password"]
+    //     ]
+    // ],204)->setStatusCode(201);
+
+    // return $data;
+        // return ["message"=>"store method called"];
+
+        $request->validate([
+            "title"=>"required|string|max:255",
+            "body"=>"required|string"
+        ]);
+        $request["author_id"]=1;
+        Post::create($request->only("title","body","author_id"));
+        return response()->json([
+            "message"=>"Post created successfully",
+            "data"=>$request->only("title","body")
+        ])->setStatusCode(201);
     }
 
     /**
@@ -51,7 +77,20 @@ class PostController extends Controller
     public function update(Request $request, string $id)
     {
         //
-        return ["message"=>"update method called"];
+        $request->validate([
+            "name"=>"required|string|max:12",
+            "email"=>"required|email",
+            "password"=>"required|string|min:6"
+        ]);
+        return response()->json([
+            "message"=>"update method called for post $id",
+            "data"=>[
+                "name"=>$request->name,
+                "email"=>$request->email,
+                "password"=>$request->password
+            ]
+        ])->setStatusCode(200);
+        // return ["message"=>"update method called"];
     }
 
     /**
@@ -59,7 +98,9 @@ class PostController extends Controller
      */
     public function destroy(string $id)
     {
-        //
-        return ["message"=>"destroy method called"];
+        //  
+        // return ["message"=>"destroy method called"];
+        return response()->noContent();
+        
     }
 }
